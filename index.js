9e3c2f25799fb1f8557d3a4cccffe4515c75a369
cmarosa2891 <cmarosa@gmail.com>
2022-08-15 08:25:20 -0300

    funciona1

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const URL = 'https://rickandmortyapi.com/api/character'
 
-window.addEventListener( 'load', () => {
-    const character = document.getElementById( 'character' )
-    const ul = document.createElement( 'ul' )
-    const data = JSON.parse( localStorage.getItem( 'characters' ) )
+window.addEventListener('load', () => {
+    const character = document.getElementById('characters')
+    const ul = document.createElement('ul')
+    const data = JSON.parse(localStorage.getItem('characters'))
 
     const getData = () => {
-        return fetch( URL )
-            .then( response => response.json() ) //convierte array en objeto 
-            .then( characters => localStorage.setItem( 'characters', JSON.stringify( characters.results.flatMap( item => ( { id: item.id, image: item.image } ) ) ) ) )
+        return fetch(URL)
+            .then(response => response.json()) //convierte array en objeto 
+            .then(characters => localStorage.setItem('characters', JSON.stringify(characters.results.flatMap(item => ({
+                id: item.id,
+                image: item.image,
+                name: item.name,
+                species: item.species,
+                status: item.status
+            })))))
     }
 
     getData()
 
-// MINUTO 48
+    data && data.map(item => {
+        console.log(item)
+        const li = document.createElement('li')
+        const img = document.createElement('img')
+        const span = document.createElement('span')
+        span.appendChild(document.createTextNode(item.name))
+        span.appendChild(document.createTextNode(' - '))
+        span.appendChild(document.createTextNode(item.species))
+        span.appendChild(document.createTextNode(' - '))
+        span.appendChild(document.createTextNode(item.status))
+        img.src = item.image
+        //li.appendChild(document.createTextNode(item.image))
+        li.appendChild(img)
+        li.appendChild(span)
+        ul.appendChild(li)
+        character.appendChild(ul)
 
-    if ( data.length ) {
-        data.map( item => {
-            console.log( item )
-            const li = document.createElement( 'li' )
-            const img = document.createElement( 'img' )
-            li.appendChild( document.createTextNode( item.image ) )
-            ul.appendChild( li )
-            character.appendChild( li )
-        } )
-
-    }
-
-
-} )
+    })
+})
